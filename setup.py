71c46d0cb427688d35f68029141fca6968918472
Mike Purvis <mpurvis@clearpathrobotics.com>
2016-02-18 15:36:39 -0500

    Add dummy entry point.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
-from distribute_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup
 
 
 setup(
     name='rosdistro_tools',
     version='0.1',
-    author='',
-    author_email='',
+    author='Mike Purvis',
+    author_email='mpurvis@clearpathrobotics.com',
     packages=['rosdistro_tools'],
     url='',
     license='See LICENSE.txt',
-    description='',
+    description='Tools for working with rosdistro repos.',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'console_scripts': [ 'rosdistro = rosdistro_tools.commands.rosdistro:main' ]
+    },
 )
